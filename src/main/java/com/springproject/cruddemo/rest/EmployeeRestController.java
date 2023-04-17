9f7658142c65cd902a99488d35e353ec8200edf9
phsu40 <phsu40@gatech.edu>
2023-04-16 20:46:16 -0700

    Implement the CRUD methods to Employee entity for the REST controller

--- src/main/java/com/springproject/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/springproject/cruddemo/rest/EmployeeRestController.java
@@ -2,9 +2,7 @@ package com.springproject.cruddemo.rest;
 
 import com.springproject.cruddemo.entity.Employee;
 import com.springproject.cruddemo.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +23,40 @@ public class EmployeeRestController {
         List<Employee> result = this.employeeService.findAll();
         return result;
     }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee employee = this.employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+        employee.setId(0);
+        Employee newEmployee = this.employeeService.save(employee);
+        return newEmployee;
+    }
+
+    // add mapping for PUT /employees - update existing employees
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee updatedEmployee = this.employeeService.save(employee);
+        return updatedEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} - delete an employee
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable int employeeId) {
+        this.employeeService.deleteById(employeeId);
+    }
+
 }
