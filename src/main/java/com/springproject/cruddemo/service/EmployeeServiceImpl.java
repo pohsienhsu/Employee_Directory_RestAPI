2378250014ff35437e2a61605623bbbebdf4c4ea
phsu40 <phsu40@gatech.edu>
2023-04-16 20:02:39 -0700

    Implement findById, save, deleteById methods to employee services and connecting it to the DAO

--- src/main/java/com/springproject/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/springproject/cruddemo/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.springproject.cruddemo.service;
 import com.springproject.cruddemo.dao.EmployeeDAO;
 import com.springproject.cruddemo.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return this.employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int employeeId) {
+        return this.employeeDAO.findById(employeeId);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return this.employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int employeeId) {
+        this.employeeDAO.deleteById(employeeId);
     }
 }
