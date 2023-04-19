e61d30b1372b19b2b866dde92342e66cbe45c268
phsu40 <phsu40@gatech.edu>
2023-04-18 17:17:10 -0700

    Add default page size for HTTP responses

--- src/main/java/com/springproject/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/springproject/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.springproject.cruddemo.dao;
 
 import com.springproject.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // JPA provided CRUD methods
